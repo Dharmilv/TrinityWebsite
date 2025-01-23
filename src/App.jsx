41d4cb8e393f676d838eceafd63002cf503e548f
Ankur Vasani <ankurvasani2@gmail.com>
2025-01-23 14:15:44 +0530

    Teams

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Example from './components/Example'
+function App() {
+
+  return (
+    <>
+      <Example />
+    </>
+  )
+}
+
+export default App
